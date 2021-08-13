c9fb6f937217a2a42af2392701abcdf8a4a3568a
Avantica123 <dattalandge001@gmail.com>
2021-08-13 22:54:47 +0530

    solve

--- Prec/settings.py
+++ Prec/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9c10-h3=u-0^pe@qu460p97db^t=n#7^)_0c=bi2hpful%dat'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['labprec.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT Prec.wsgi:application
