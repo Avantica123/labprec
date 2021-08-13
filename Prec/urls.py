68257a6645c362cc0ba13b6b7f3bf67029fbcce7
Avantica123 <dattalandge001@gmail.com>
2021-08-14 00:32:24 +0530

    fhfhf

--- Prec/urls.py
+++ Prec/urls.py
@@ -27,7 +27,7 @@ from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^upload/images/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path("",views.index,name="home"),
     path("signup/",views.signup, name="signup"),
